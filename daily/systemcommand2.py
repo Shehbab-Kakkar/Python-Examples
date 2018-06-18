42eba48813b335b7357bfd1847ef15aab49c6e66
root <root@centos7.se2.ipsoft.com>
2018-06-18 11:27:12 +0200

    updates

--- daily/systemcommand2.py
+++ daily/systemcommand2.py
@@ -3,6 +3,6 @@ import os
 import subprocess
 import time
 import os
-#os.system("rsync -avrz /root/Python root@shehbab.se2.ipsoft.com:/home/lsoni/")
+#os.system("rsync -avrz /root/Python root@shehbab.example.com:/home/lsoni/")
 #os.system("ls -la /root/Python | grep -i function")
-os.system("scp -rv /root/Python root@shehbab.se2.ipsoft.com:/home/lsoni/Python")
+os.system("scp -rv /root/Python root@shehbab.example.com:/home/lsoni/Python")
